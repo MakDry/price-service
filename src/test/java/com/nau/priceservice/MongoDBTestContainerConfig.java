7ff815d4c0221342d33a02997d13ebbc02ea0517
Maksym Dryzhenko <93612326+MakDry@users.noreply.github.com>
2024-03-13 20:35:37 +0200

    Service Layer Unit Tests | DAO layer MongoDB configuration (#15)
    * Service layer covered with unit tests
    
    * Created basic MongoDB config for repository testing

---
+++ src/test/java/com/nau/priceservice/MongoDBTestContainerConfig.java
@@ -0,0 +1,20 @@
+package com.nau.priceservice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+@Configuration
+@EnableMongoRepositories
+public class MongoDBTestContainerConfig {
+    @Container
+    public static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest")
+            .withExposedPorts(27017);
+
+    static {
+        mongoDBContainer.start();
+        int mappedPort = mongoDBContainer.getMappedPort(27017);
+        System.setProperty("mongodb.container.port", String.valueOf(mappedPort));
+    }
+}
