5564d4ad7dceaef2d26b47b824977bdf57b1807e
MakDry <fakecounter050@gmail.com>
2024-03-13 23:44:05 +0200

    Repositories rewrite to MongoRepository interface implementation

--- src/main/java/com/nau/priceservice/data/dao/interfaces/ProductDao.java
+++
@@ -1,8 +0,0 @@
-package com.nau.priceservice.data.dao.interfaces;
-
-import com.nau.priceservice.data.dao.Dao;
-import com.nau.priceservice.data.entity.ProductEntity;
-
-public interface ProductDao<T extends ProductEntity, K extends String> extends Dao<T, K> {
-    boolean isExists(K k);
-}
